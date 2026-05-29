a5a7c3f0a0bba2e5684f6b522f315cd22c29128e
xkadzama <kartoev.husein@gmail.com>
2026-05-29 14:48:19 +0300

    Созданы роутер и пути для товаров

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 
 from app.routers import categories
-
+from app.routers import products
 
 app = FastAPI(title='FastAPI интернет магазин', version='0.1.0')
 
 app.include_router(categories.router)
-
+app.include_router(products.router)
 
 @app.get('/')
 async def root():
